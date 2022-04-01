a138482ad61867759a3c556e4e9573473381b4b5
ErinAfrin <erinafrin33@gmail.com>
2022-04-01 10:27:43 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# java_problemslove_3
---
+++ problem2.js
@@ -0,0 +1,5 @@
+const animals = ['rifat', 'shahin', 'rakib', 'redwan', 'mukta'];
+animals.splice(0,1)
+animals.splice(0,1)
+animals.splice(2,1)
+console.log(animals)
